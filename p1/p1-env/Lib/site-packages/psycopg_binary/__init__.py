62cd8b359ecc9a9ca90b8b86d70c5062504c8ee6
rde-ea <howdoyouturnthison94>
2023-01-05 09:49:32 +0100

    add folder for 1st project

---
+++ p1/p1-env/Lib/site-packages/psycopg_binary/__init__.py
@@ -0,0 +1,44 @@
+"""
+psycopg -- PostgreSQL database adapter for Python -- C optimization package
+"""
+
+
+# start delvewheel patch
+def _delvewheel_init_patch_1_1_4():
+    import ctypes
+    import os
+    import platform
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'psycopg_binary.libs'))
+    is_pyinstaller = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
+    is_conda_cpython = platform.python_implementation() == 'CPython' and (hasattr(ctypes.pythonapi, 'Anaconda_GetVersion') or 'packaged by conda-forge' in sys.version)
+    if sys.version_info[:2] >= (3, 8) and not is_conda_cpython or sys.version_info[:2] >= (3, 10):
+        if not is_pyinstaller or os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+    else:
+        load_order_filepath = os.path.join(libs_dir, '.load-order-psycopg_binary-3.1.6')
+        if not is_pyinstaller or os.path.isfile(load_order_filepath):
+            with open(os.path.join(libs_dir, '.load-order-psycopg_binary-3.1.6')) as file:
+                load_order = file.read().split()
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                if not is_pyinstaller or os.path.isfile(lib_path):
+                    ctypes.WinDLL(lib_path)
+
+
+_delvewheel_init_patch_1_1_4()
+del _delvewheel_init_patch_1_1_4
+# end delvewheel patch
+
+
+
+# Copyright (C) 2020 The Psycopg Team
+
+import sys
+
+# This package shouldn't be imported before psycopg itself, or weird things
+# will happen
+if "psycopg" not in sys.modules:
+    raise ImportError("the psycopg package should be imported before psycopg_binary")
+
+from .version import __version__ as __version__  # noqa
